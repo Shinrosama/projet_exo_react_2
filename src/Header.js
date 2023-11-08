e9be2a0a4c07e512ceac142ae47c2fa966ed775e
hamid Hazzab <hamid.hazzab@lapiscine.pro>
2023-11-08 10:09:38 +0100

    add an if else in the header for exercise

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,16 @@
+const isUserLogged = false;
+
+
 function Header(){
     return(
         <header>
             <h1>Exo 3 mon titre</h1>
+
+            {isUserLogged?(
+                <li>Shinro</li>
+            ):(
+                <li>Not logged</li>
+            )}
             <ul>
                 <li>Home</li>
                 <li>Product</li>
